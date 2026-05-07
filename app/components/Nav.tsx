c3df5adb336a3dba45e35e0278b12e82d578b65b
Naeem Carr <mogamatnaeemcarr@gmail.com>
2026-05-07 11:04:26 +0200

    feat/finished hero and nav animations

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -5,17 +5,26 @@ import { AnimatePresence, motion } from 'motion/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { Activity } from 'react';
+
 export default function Nav() {
 	const [isOpen, setOpen] = React.useState(false);
+	const [scrolled, setScrolled] = React.useState(false);
+
+	React.useEffect(() => {
+		const handleScroll = () => {
+			setScrolled(window.scrollY > 10);
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 
 	return (
-		<div className='nav'>
+		<div className={`nav ${scrolled ? 'scrolled' : ''}`}>
 			<nav className='max-width '>
 				<Image src='/images/logo.svg' alt='logo' width={130} height={40} />
 				<ul>
 					<li>
-						<Link href='#hero'>About</Link>
+						<Link href='#hero'>About us</Link>
 					</li>
 					<li>
 						<Link href='#hero'>Services</Link>
@@ -45,10 +54,24 @@ export default function Nav() {
 						transition={{ duration: 0.3 }}
 						className='mobile-nav'
 					>
-						awe
+						<ul>
+							<li>
+								<Link href='#hero'>About</Link>
+							</li>
+							<li>
+								<Link href='#hero'>Services</Link>
+							</li>
+							<li>
+								<Link href='#hero'>Reviews</Link>
+							</li>
+							<li>
+								<Link href='#hero'>Contact</Link>
+							</li>
+						</ul>
 					</motion.div>
 				)}
 			</AnimatePresence>
+			{scrolled && <div>scroll to top</div>}
 		</div>
 	);
 }
